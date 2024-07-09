1c48c63dd1584be9a1f786b626c5ec2cc488b243
atilaspo <santiago.ortiz@hotmail.com.ar>
2024-07-09 15:13:13 +1000

    Added session_check.php

---
+++ session_check.php
@@ -0,0 +1,11 @@
+<?php
+session_start();
+
+if (!isset($_SESSION['user_id'])) {
+    header("Location: login.php");
+    exit();
+}
+
+$user_id = $_SESSION['user_id'];
+$user_role = $_SESSION['role'];
+?>
